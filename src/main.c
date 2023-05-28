ee38de113b087ab5a7ef913755b71bbc0b2a0b93
giomba <giomba@glgprograms.it>
2023-05-28 17:33:11 +0200

    Add minor customization to Criterion config.

--- src/main.c
+++ src/main.c
@@ -9,20 +9,25 @@
 #define LOG_LEVEL LOG_LVL_INFO
 #include "log.h"
 
-int main() {
-    LOG_INFO("CEDA\n");
-
+int main(int argc, char *argv[]) {
     int ret = 0;
 
 #ifdef CEDA_TEST
+    LOG_INFO("CEDA Test\n");
     criterion_options.color = true;
     criterion_options.full_stats = true;
+    criterion_options.timeout = 1;
 
     struct criterion_test_set *set = criterion_initialize();
-    ret = !criterion_run_all_tests(set);
+    if (criterion_handle_args(argc, argv, true))
+        ret = !criterion_run_all_tests(set);
     criterion_finalize(set);
 
 #else
+    LOG_INFO("CEDA Emulator\n");
+
+    (void)argc;
+    (void)argv;
     ceda_init();
     ceda_run();
 #endif
