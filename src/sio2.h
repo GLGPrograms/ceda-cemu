cd2e5e81ee88a6c022faed39b8ccec77b9a2b84b
giomba <giomba@glgprograms.it>
2025-01-03 19:07:36 +0100

    Refactor SIO/2 to export channels defines.

--- src/sio2.h
+++ src/sio2.h
@@ -4,7 +4,20 @@
 #include "module.h"
 #include "type.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include <Z80.h>
+
+typedef enum sio_channel_idx_t {
+    SIO_CHANNEL_A = 0,
+    SIO_CHANNEL_B = 1,
+
+    SIO_CHANNEL_CNT,
+} sio_channel_idx_t;
+
+typedef bool (*sio_channel_try_read_t)(uint8_t *c);
+typedef bool (*sio_channel_try_write_t)(uint8_t c);
 
 void sio2_init(CEDAModule *mod);
 uint8_t sio2_in(ceda_ioaddr_t address);
